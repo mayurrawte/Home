3b0ccd6fe7788d194fe086f58db7d9bf40b8283c
Someone <someone-loves-gnu@localhost.localdomain>
2017-07-14 03:44:54 +0530

    button ki jugaad

--- RailMitraBot/views.py
+++ RailMitraBot/views.py
@@ -42,6 +42,8 @@ class RailMitraView(generic.View):
                         trainNo, Station = str(message['message']['text']).split()
                         data = json.loads(railapi.getStationsFromTrainNumber(trainNo))
                         btnar = []
+                        ob = open('test.txt', 'w+')
+                        ob.write(json.dumps(message))
                         def pps(k, v):
                             btnar.append({"type": "postback", "title": v, "payload": k})
                         [pps(k, v) for k, v in data['stations'].iteritems() if Station in v.lower()]
@@ -59,7 +61,7 @@ class RailMitraView(generic.View):
 def post_button(fbid, btnarr):
     post_message_url = 'https://graph.facebook.com/v2.9/me/messages?access_token=EAAcQ73ZA7PfgBALIekJFW8zudPg9XKdG7oNGA2aR33sRqKEppHrVBY5UCGsxNHqe2PyI4qRy9yoJa3UoUJ9NCvoPl5t6SLxV5OYmEX4GnHtZACX0SBq6N29YdVQLDTqX0SE1FfhDNSdxbWGEk1ZB9l1MC6DxZCqygNaROQF3IZA4pJd69rqvj'
     #response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": "maakda nai chal raha"}})
-    response_msg = json.dumps({"recipient":{"id": fbid }, "message":{"attachment":{"type":"template", "payload":{"template_type":"button", "text":"What do you want to do next?", "buttons":btnarr } } } })
+    response_msg = json.dumps({"recipient":{"id": fbid }, "message":{"attachment":{"type":"template", "payload":{"template_type":"button", "text":"What do you want to do next?", "buttons": btnarr } } } })
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
     print(status.json())
 

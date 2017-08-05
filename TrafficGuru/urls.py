e0559b6e193aef5a77002f04a32696d73872735c
Someone <someone-loves-gnu@localhost.localdomain>
2017-08-05 21:20:42 +0530

    TrafficGuru: Adding logics

--- TrafficGuru/urls.py
+++ TrafficGuru/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from TrafficGuru import views
 
 urlpatterns = [
-    url(r'^7a8bc5f20d6c86b3021a74a4a1bca1bbe411ea6b9f04628f6a/?$', views.TrafficGuruView.as_view()),
-    url(r'^privacy', views.privacy)
+    url(r'^7a8bc5f20d6c86b3021a74a4a1bca1bbe411ea6b9f04628f6a/?$', views.TrafficGuruView.as_view())
 ]
